a5f748ded53f537a72114e1da24c03fd87e25d41
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-07-19 20:40:48 +0200

    comment update

--- memory calculation.py
+++ memory calculation.py
@@ -1,6 +1,6 @@
 def sum(n):
     return n*(n+1)/2
-
+#Space Complexity: 0(1), Auxsillary Space = 0(1), Linear space:
 def arraysum(a):
     sum = 0
     for i in a:
@@ -8,6 +8,8 @@ def arraysum(a):
     return sum
 b = [1,2,1,2]
 print(arraysum(b))
+# With the size of the array, the space increases...
+#Space Complexity 0(n), Auxsillary Space: 0(1)
 def summ(n):
     if(n<=0):
         return
